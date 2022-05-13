35e7cfb675668a74922fc51a5f0ceda646e9484a
dk <ssddgg99@naver.com>
2022-05-13 17:03:55 +0900

    s

--- dk.c
+++ dk.c
@@ -1,13 +1,19 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
 
 int main()
 {
-	cout << "hi";
+	char buf[] = "QBC";
+
+	cout << strcmp(buf, "ABC");
+
+	//앞에있는 문자열이 사전순으로 더 크면 1
+	// 앞에 있는 문자열이 사전순으로 더 작으면 -1
+	//완전 똑같으면 0
 
 
 
 
 	return 0;
 }
-
